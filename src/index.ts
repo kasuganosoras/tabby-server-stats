58f3d3a86dbec9dd2ddaeccc0793e7445eed0876
smhong <smhong@ntels.com>
2026-01-08 12:11:24 +0900

    Revert "Refactor bottom bar alignment and enhance server stats polling logic"
    This reverts commit 4132609048dec11d3b23a56bbeb70f228b694c4c.

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,6 @@ type TabInstance = {
     timerId: any
     collector: () => Promise<any>
     state: any
-    configSub: any
 }
 
 const LOG_PATH = path.join(os.tmpdir(), 'tabby-server-stats.log')
@@ -378,17 +377,11 @@ export default class ServerStatsModule {
         const runPoll = async () => {
             const isEnabled = this.config.store.plugin?.serverStats?.enabled
             const displayMode = this.getDisplayMode()
-            if (!isEnabled || displayMode !== 'bottomBar') {
-                if ((barRef.instance as any).hideExternal) {
-                    (barRef.instance as any).hideExternal()
-                }
-                return
-            }
             const result = await collector()
             if (!result) {
                 return
             }
-            if (!result.session) {
+            if (!isEnabled || displayMode !== 'bottomBar' || !result.session) {
                 if ((barRef.instance as any).hideExternal) {
                     (barRef.instance as any).hideExternal()
                 }
@@ -410,33 +403,15 @@ export default class ServerStatsModule {
             }
         }
 
-        const syncOnConfigChange = () => {
-            const isEnabled = this.config.store.plugin?.serverStats?.enabled
-            const displayMode = this.getDisplayMode()
-            if (!isEnabled || displayMode !== 'bottomBar') {
-                if ((barRef.instance as any).hideExternal) {
-                    (barRef.instance as any).hideExternal()
-                }
-                return
-            }
-            if ((barRef.instance as any).setExternalLoading) {
-                (barRef.instance as any).setExternalLoading(true)
-            }
-            runPoll()
+        if ((barRef.instance as any).setExternalLoading) {
+            (barRef.instance as any).setExternalLoading(true)
         }
-
-        syncOnConfigChange()
+        runPoll()
         const timerId = window.setInterval(runPoll, 3000)
-        const configSub = this.config.changed$?.subscribe(() => {
-            syncOnConfigChange()
-        })
 
         const teardown = () => {
             if (timerId) {
                 clearInterval(timerId)
-            }
-            if (configSub && typeof configSub.unsubscribe === 'function') {
-                configSub.unsubscribe()
             }
             try {
                 barRef.destroy()
@@ -451,7 +426,7 @@ export default class ServerStatsModule {
             sshTabEl.classList.remove('server-stats-tab')
         }
 
-        this.tabInstances.set(sshTabEl, { teardown, timerId, collector, state, configSub })
+        this.tabInstances.set(sshTabEl, { teardown, timerId, collector, state })
         this.attachedTabs.add(sshTabEl)
     }
 
